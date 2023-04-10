e346de70569c995140ff44542085993bf0aa2720
TiagoMarante <TiagoMarante@ORYON-5750-TFM>
2023-04-10 15:25:50 +0100

    Making migrations easier and support data changes

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,14 +1,12 @@
 
+from uuid import UUID
 from sqlmodel import Field
 from app.models.base_model import BaseModel
 
 class User(BaseModel, table=True):
     email: str = Field(unique=True)
     password: str = Field()
-
-    #password2: str = Field()
     user_token: str = Field(unique=True)
-
     name: str = Field(default=None, nullable=True)
     is_active: bool = Field(default=True)
     is_superuser: bool = Field(default=False)
